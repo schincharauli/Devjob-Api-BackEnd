57e4575c7089f0549877996c9a6416b14cd5749f
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-07-31 11:50:01 +0400

    added pool with server details

--- config/sql.js
+++ config/sql.js
@@ -1,4 +1,12 @@
 import pgk from "pg";
 const { Pool } = pgk;
 
-const pool = new Pool();
+const pool = new Pool({
+  host: "dpg-cj3mcut9aq0e0q00a7vg-a",
+  port: 5432,
+  Database: "devjobs_api",
+  user: "devjobs_api_user",
+  password: "9h5kJYkVzL18902mucAqzNkw4Qq0yQ89",
+});
+
+export const CreateImageTable = async () => {};
