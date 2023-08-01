509f38ca615c48f1b31a7a1393c29c454ccb36bf
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-08-01 15:04:38 +0400

    added router to get request

---
+++ src/routes/jobs_router.js
@@ -0,0 +1,8 @@
+import express from "express";
+
+import { getAllDescriptions } from ".../controllers/jobs-controller.js";
+const jobsRouter = express.Router();
+
+jobsRouter.get("/", getAllDescriptions);
+
+export default jobsRouter;
