257aca9f4411a450b72b675d11039859ee0027f9
schincharauli <52008243+schincharauli@users.noreply.github.com>
2023-08-01 15:11:14 +0400

    minor changes

--- src/routes/jobs_router.js
+++ src/routes/jobs_router.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getAllDescriptions } from ".../controllers/jobs-controller.js";
+import { getAllDescriptions } from "../controllers/jobs-controller.js";
 const jobsRouter = express.Router();
 
 jobsRouter.get("/", getAllDescriptions);
